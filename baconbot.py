d62890c5ba3165a19cfbb07a3aae3bde104c517e
Matt Bailey <matt@matt-bailey.co.uk>
2017-04-30 22:16:25 +0100

    Move channel to environment variable.

--- baconbot.py
+++ baconbot.py
@@ -2,11 +2,12 @@ import os
 from slackclient import SlackClient
 
 slack_token = os.environ["SLACK_API_TOKEN"]
+slack_channel = os.environ["SLACK_CHANNEL"]
 
 sc = SlackClient(slack_token)
 
 sc.api_call(
     "chat.postMessage",
-    channel="#wibble",
+    channel=slack_channel,
     text="Hello from baconbot!"
 )
