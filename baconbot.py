11c77ab3a9e5db1318c0ee1dd7386fd50575fb28
Matt Bailey <matt@matt-bailey.co.uk>
2017-05-02 23:12:05 +0100

    Read from RTM API and log out everything we get.

--- baconbot.py
+++ baconbot.py
@@ -1,4 +1,5 @@
 import os
+import time
 from slackclient import SlackClient
 
 slack_token = os.environ["SLACK_API_TOKEN"]
@@ -6,8 +7,28 @@ slack_channel = os.environ["SLACK_CHANNEL"]
 
 sc = SlackClient(slack_token)
 
-sc.api_call(
-    "chat.postMessage",
-    channel=slack_channel,
-    text="Hello from baconbot!"
-)
+
+def parse_slack_output(slack_rtm_output):
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output:
+                # return text after the @ mention, whitespace removed
+                return output['text'], output['channel']
+    return None, None
+
+
+def handle_command(command, channel):
+    print(command, channel)
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1
+    if sc.rtm_connect():
+        print("baconbot connected and running!")
+        while True:
+            command, channel = parse_slack_output(sc.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
