a35685a391ed42fde94d5ad68b9bebbbfb317a44
Matt Bailey <matt@matt-bailey.co.uk>
2017-05-03 21:56:53 +0100

    Programmatically obtain the bot ID based on the bot name and only log out messages directed at the bot itself.

--- baconbot.py
+++ baconbot.py
@@ -4,16 +4,31 @@ from slackclient import SlackClient
 
 slack_token = os.environ["SLACK_API_TOKEN"]
 slack_channel = os.environ["SLACK_CHANNEL"]
+bot_name = os.environ["BOT_NAME"]
 
 sc = SlackClient(slack_token)
 
 
-def parse_slack_output(slack_rtm_output):
+def get_bot_id():
+    api_call = sc.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == bot_name:
+                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+                return user.get('id')
+    else:
+        print("could not find bot user with the name " + BOT_NAME)
+        return None
+
+
+def parse_slack_output(bot_id, slack_rtm_output):
+    at_bot_id = "<@" + bot_id + ">"
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            if output and 'text' in output:
-                # return text after the @ mention, whitespace removed
+            if output and 'text' in output and at_bot_id in output['text']:
                 return output['text'], output['channel']
     return None, None
 
@@ -21,12 +36,14 @@ def parse_slack_output(slack_rtm_output):
 def handle_command(command, channel):
     print(command, channel)
 
+
 if __name__ == "__main__":
+    bot_id = get_bot_id()
     READ_WEBSOCKET_DELAY = 1
     if sc.rtm_connect():
         print("baconbot connected and running!")
         while True:
-            command, channel = parse_slack_output(sc.rtm_read())
+            command, channel = parse_slack_output(bot_id, sc.rtm_read())
             if command and channel:
                 handle_command(command, channel)
             time.sleep(READ_WEBSOCKET_DELAY)
