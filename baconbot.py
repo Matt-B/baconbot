f54c93939d8cc266c4fed8169b743be94f091904
Matt Bailey <matt@matt-bailey.co.uk>
2017-05-07 22:10:18 +0100

    Add ability to show the menu.

--- baconbot.py
+++ baconbot.py
@@ -11,6 +11,12 @@ pushbullet_api_key = os.environ["PUSHBULLET_API_KEY"]
 sc = SlackClient(slack_token)
 pb = PushBullet(pushbullet_api_key)
 
+menu = {"1": "Bacon Baguette (Small)",
+        "2": "Bacon Baguette (Large)",
+        "3": "Bacon and Sausage Baguette (Small)",
+        "4": "Bacon and Sausage Baguette (Large)",
+        "5": "Breakfast Baguette (Bacon/Egg/Sausage) (Large)"}
+
 
 def get_bot_id():
     api_call = sc.api_call("users.list")
@@ -41,6 +47,10 @@ def handle_command(command, channel):
     if "confirm" in command:
         pb.push_note(bot_name, channel + ":" + command)
         response = "Order pushed."
+    elif "menu" in command:
+        response = ""
+        for key, value in menu.items():
+            response = response + "\n" + key + ": " + value
     elif "order" in command:
         response = "Here, I would take an order."
     else:
