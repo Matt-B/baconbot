1dcb8f0f92b92c8dffaba379a9105ef9a307053c
Matt Bailey <matt@matt-bailey.co.uk>
2017-05-08 22:30:40 +0100

    Add ability to place an order - the bot will hold it in memory for now.

--- baconbot.py
+++ baconbot.py
@@ -1,5 +1,6 @@
 import os
 import time
+import re
 from slackclient import SlackClient
 from pushbullet import PushBullet
 
@@ -11,11 +12,13 @@ pushbullet_api_key = os.environ["PUSHBULLET_API_KEY"]
 sc = SlackClient(slack_token)
 pb = PushBullet(pushbullet_api_key)
 
-menu = {"1": "Bacon Baguette (Small)",
-        "2": "Bacon Baguette (Large)",
-        "3": "Bacon and Sausage Baguette (Small)",
-        "4": "Bacon and Sausage Baguette (Large)",
-        "5": "Breakfast Baguette (Bacon/Egg/Sausage) (Large)"}
+menu = {1: "Bacon Baguette (Small)",
+        2: "Bacon Baguette (Large)",
+        3: "Bacon and Sausage Baguette (Small)",
+        4: "Bacon and Sausage Baguette (Large)",
+        5: "Breakfast Baguette (Bacon/Egg/Sausage) (Large)"}
+
+placed_orders = {}
 
 
 def get_bot_id():
@@ -38,21 +41,36 @@ def parse_slack_output(bot_id, slack_rtm_output):
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and at_bot_id in output['text']:
-                return output['text'], output['channel']
-    return None, None
+                return output['text'], output['channel'], output['user']
+    return None, None, None
 
 
-def handle_command(command, channel):
-    print(command, channel)
+def parse_order(order_command, user):
+    ordered_items = [int(s) for s in order_command.split() if s.isdigit()]
+    items = []
+    for item in ordered_items:
+        if item <= len(menu):
+            items.append(menu.get(item))
+    placed_orders[user] = items
+    return items
+
+
+def handle_command(command, channel, user):
     if "confirm" in command:
         pb.push_note(bot_name, channel + ":" + command)
         response = "Order pushed."
     elif "menu" in command:
         response = ""
         for key, value in menu.items():
-            response = response + "\n" + key + ": " + value
+            response = response + "\n" + str(key) + ": " + value
     elif "order" in command:
-        response = "Here, I would take an order."
+        order = parse_order(command, user)
+        if len(order) > 0:
+            response = "<@" + user + "> You have ordered:\n"
+            for item in order:
+                response += item + "\n"
+        else:
+            response = "No menu items matched.\nPlease enter your choice(s) separated by a space."
     else:
         response = "I have no idea what you are asking me."
 
@@ -65,9 +83,9 @@ if __name__ == "__main__":
     if sc.rtm_connect():
         print(bot_name + " connected and running!")
         while True:
-            command, channel = parse_slack_output(bot_id, sc.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
+            command, channel, user = parse_slack_output(bot_id, sc.rtm_read())
+            if command and channel and user:
+                handle_command(command, channel, user)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
